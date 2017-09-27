c5a8fd4bac6ae5a748db49411f207eb864579d1e
Wei Zhu <yesmeck@gmail.com>
2017-09-27 11:39:18 +0800

    Simplify

--- src/Hello.js
+++ src/Hello.js
@@ -1,27 +1,7 @@
 import React from 'react';
 
-export default class Hello extends React.Component {
-  constructor(props) {
-    super(props);
-
-    this.state = {
-      show: props.show,
-    }
-  }
-
-  componentWillReceiveProps(nextProps) {
-    this.setState(() => ({
-      show: nextProps.show,
-    }));
-  }
-
-  render() {
-    const { show } = this.state;
-    return (
-      <div>
-        {show ? <span>hello</span> : null}
-      </div>
-    );
-  }
-}
-
+export default ({ show }) => (
+  <div>
+    {show ? <span>hello</span> : null}
+  </div>
+);
